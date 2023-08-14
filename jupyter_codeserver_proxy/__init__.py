efe8eea2a9c9b7a4289b1c3186e4c7da8dd50579
Charles Teague <cteague@gmail.com>
2023-08-14 13:27:06 -0400

    Update __init__.py

--- jupyter_codeserver_proxy/__init__.py
+++ jupyter_codeserver_proxy/__init__.py
@@ -1,42 +1,41 @@
 import os
 import shutil
-import logging
 
 
-logger = logging.getLogger(__name__)
-logger.setLevel('INFO')
+def setup_vscode():
+    def _get_vscode_cmd(port):
+        executable = "code-server"
+        if not shutil.which(executable):
+            raise FileNotFoundError("Can not find code-server in PATH")
+        
+        # Start vscode in CODE_WORKINGDIR env variable if set
+        # If not, start in 'current directory', which is $REPO_DIR in mybinder
+        # but /home/jovyan (or equivalent) in JupyterHubs
+        working_dir = os.getenv("CODE_WORKINGDIR", ".")
 
+        extensions_dir = os.getenv("CODE_EXTENSIONSDIR", None)
+        cmd = [
+            executable,
+            "--auth",
+            "none",
+            "--disable-telemetry",
+            "--port=" + str(port),
+        ]
 
-def setup_codeserver():
-    """Setup commands and icon paths and return a dictionary compatible
-    with jupyter-server-proxy.
-    """
-    def _get_icon_path():
-        return os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), 'icons', 'vscode.svg'
-    )
+        if extensions_dir:
+            cmd += ["--extensions-dir", extensions_dir]
 
-    # Make sure executable is in $PATH
-    def _get_codeserver_command(port):
-        executable = shutil.which('codeserver')
-        if not executable:
-            raise FileNotFoundError('Can not find codeserver executable in $PATH')
-        # Create theia working directory
-        home_dir = os.environ.get('HOME') or '/home/jovyan'
-        working_dir = f'{home_dir}/codeserver'
-        if not os.path.exists(working_dir):
-            os.makedirs(working_dir)
-            logger.info("Created directory %s" % working_dir)
-        else:    
-            logger.info("Directory %s already exists" % working_dir)
-        return ['codeserver']
-    
+        cmd.append(working_dir)
+        return cmd
+
     return {
-        'command': '_get_codeserver_command',
-        'timeout': 20,
-        'new_browser_tab': True,
-        'launcher_entry': {
-            'title': 'VSCode',
-            'icon_path': _get_icon_path()
+        "command": _get_vscode_cmd,
+        "timeout": 300,
+        "new_browser_tab": True,
+        "launcher_entry": {
+            "title": "VS Code",
+            "icon_path": os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "icons", "vscode.svg"
+            ),
         },
     }
